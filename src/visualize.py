6f7260408201fc4f1793582af624e8cfaf5778f9
Nizeyumukiza <jpnizeyumukiza@gmail.com>
2023-04-04 21:44:11 -0700

    added the bar chats

--- src/visualize.py
+++ src/visualize.py
@@ -12,6 +12,7 @@ args = parser.parse_args()
 import os
 import json
 from collections import Counter,defaultdict
+import matplotlib.pyplot as plt
 
 # open the input path
 with open(args.input_path) as f:
@@ -24,5 +25,13 @@ if args.percent:
 
 # print the count values
 items = sorted(counts[args.key].items(), key=lambda item: (item[1],item[0]), reverse=True)
+top_ten = sorted(items[:11] or items.most_common(10), reverse=False)
+keys = list(top_ten.keys())
+values = list(top_ten.values())
+plt.bar(keys, values)
+plt.xlabel('data keys')
+plt.ylabel('frequency of the keys in the data')
+plt.title("Bar chat of key and it's frequency.")
+plt.savefig('../visuals/figs',dpi='figure', format='png')
 for k,v in items:
     print(k,':',v)
